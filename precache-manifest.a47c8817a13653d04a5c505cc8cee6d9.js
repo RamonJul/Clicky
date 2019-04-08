38623e151ef7c7afab72d529413c2f4071a16524
RamonJul <ramoncarlosjulian@gmail.com>
2019-04-07 21:03:29 -0400

    Updates

---
+++ precache-manifest.a47c8817a13653d04a5c505cc8cee6d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1726ed0b9997b87a72d7",
+    "url": "/Clicky/static/js/runtime~main.a36ce3a7.js"
+  },
+  {
+    "revision": "2c39c3125820bafbac89",
+    "url": "/Clicky/static/js/main.b9d7fc32.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/Clicky/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "2c39c3125820bafbac89",
+    "url": "/Clicky/static/css/main.1ac57eef.chunk.css"
+  },
+  {
+    "revision": "24b362d8436cd1437e9fde83d1b71a76",
+    "url": "/Clicky/index.html"
+  }
+];
